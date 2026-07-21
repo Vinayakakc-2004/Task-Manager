d100d416b52cb4d878bccdb7edde84b986818889
Vinayaka K C <vinayakakc28@gmail.com>
2026-07-21 20:40:35 +0530

    Updated ALLOWED_HOSTS

--- taskproject/taskproject/settings.py
+++ taskproject/taskproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4_z@vrb!+ensnm@cpsy2q8-h$=di5qfh0x0m)j$4mv@7%x6395
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
