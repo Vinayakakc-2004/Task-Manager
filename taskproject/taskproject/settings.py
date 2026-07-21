a3782c258503f59f7726b7a7a0734ec8bd316b82
Vinayaka K C <vinayakakc28@gmail.com>
2026-07-21 20:44:05 +0530

    Changed database to sqlite

--- taskproject/taskproject/settings.py
+++ taskproject/taskproject/settings.py
@@ -73,17 +73,16 @@ WSGI_APPLICATION = 'taskproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'taskdb',
-        'USER': 'root',
-        'PASSWORD': 'Anvitha@-123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
